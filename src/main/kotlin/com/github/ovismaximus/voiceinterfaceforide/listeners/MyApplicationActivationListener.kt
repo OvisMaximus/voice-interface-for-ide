3829a442b04d7ae2973818e99f16ad8e6b8e9568
GitHub Action <action@github.com>
2023-07-20 12:15:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/ovismaximus/voiceinterfaceforide/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ovismaximus/voiceinterfaceforide/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ovismaximus.voiceinterfaceforide.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
